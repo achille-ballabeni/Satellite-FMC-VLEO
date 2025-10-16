301182581b1a4b6be054b6f4bb0fb2a84d67b75a
achille-ballabeni <114033785+achille-ballabeni@users.noreply.github.com>
2025-10-16 18:14:00 +0200

    Script to run multiple tests.

--- src/OF_Analysis/OFtests.m
+++ src/OF_Analysis/OFtests.m
@@ -1,48 +1,120 @@
-% Buongiorno,
-% dando seguito alla discussione di ieri pomeriggio, lascio il link a due
-% funzioni di Matlab per inserire il motion-blur nelle immagini usate per
-% calcolare il LoS motion:
-%
-% https://www.mathworks.com/help/images/ref/fspecial.html
-%
-% https://it.mathworks.com/help/images/ref/imfilter.html
-%
-% La prima dovrebbe creare la matrice di convoluzione, la seconda serve ad
-% applicare il filtro alla data immagine. Il motion blur viene specificato
-% tramite numero di pixel e direzione rispetto all'orizzontale. Credo
-% quindi sia abbastanza immediato implementarlo utilizzando le informazioni
-% già disponibili nel vostro modello per il calcolo dello shift tra la
-% coppia di immagini. Di primo acchito, credo che l'intensità del motion
-% blur si ottenga semplicemente scalando lo shift tra le due immagini per
-% il rapporto (assumed_exposure_time/frame rate), come ci dicevamo l'altro
-% giorno.
-%
-% Oltre a questo, sarebbe a mio avviso interessante fare un breve studio su
-% come l'algoritmo di optical flow scali (in termini di
-% latency_vs_accuracy) al diminuire della dimensione dell'immagine
-% utilizzata. Quest'ultima la testerei ipotizzando di fare un cropping
-% centrato nel centro dell'immagine, ovvero tagliando "i bordi"  passando
-% ad es da 1920x1080 a 960x540 –> 480x270.
+% 1920x1080 a 960x540 –> 480x270.
 %
 % Spero quanto sopra sia d'aiuto.
+clc, clear
 
-u = 100;
-v = 1.9;
-exposure_time = 1/500;
+u = -155.2630; % Mean values from simulation, nadir pointing, SSO orbit
+v = 8.9959; % Mean values from simulation, nadir pointing, SSO orbit
+exposure_time = 1/1000;
 dt = 0.1;
 
-image = imread("D:\AKO\UNI_AERO\Tesi_Magistrale\VLEO_numerical_simulator\src\media\full_img.png");
+blur_vec       = [false,false,true,true];
+continuity_vec = [false,true,true,false];
+resolution = [[2560,2560];
+              [1440,2560];
+              [1920,1080];
+              [960,540];
+              [480,270]];
 
-blur_len = sqrt(u^2+v^2)*exposure_time/dt;
-blur_angle = rad2deg(atan(v/u));
-H = fspecial("motion",blur_len,blur_angle);
-blurred_img = imfilter(image,H,"replicate");
+n = size(resolution, 1);
+m = length(blur_vec);
 
-targetSize = [480,270];
-r = centerCropWindow2d(size(blurred_img),targetSize);
-cropped = imcrop(blurred_img,r);
+substruct = repmat(struct('blur', [], ...
+                          'continuity', [], ...
+                          'u_est', [], ...
+                          'v_est', [], ...
+                          'mae_u', [], ...
+                          'mae_v', [], ...
+                          'std_u', [], ...
+                          'std_v', [], ...
+                          'time', [], ...
+                          'fps', []), m, 1);
+imaging = repmat(struct('resolution', [], ...
+                        'u_real', [], ...
+                        'v_real', [], ...
+                        'data', substruct), n, 1);
 
-[original_img, shifted_img] = img_shift(cropped,u,v);
-tic
-[u_est,v_est] = OF(original_img,shifted_img,10)
-toc
+                        
+
+imgFiles = dir(fullfile("D:\AKO\UNI_AERO\Tesi_Magistrale\VLEO_numerical_simulator\src\media\test_db",'*.jpg'));
+
+% Loop through resolutions
+for i = 1:n
+    fprintf("#### Resolution: %dx%d #### \n",resolution(i,2),resolution(i,1))
+    imaging(i).resolution = resolution(i,:);
+    imaging(i).u_real = u;
+    imaging(i).v_real = v;
+    
+    % Loop through settings
+    for h = 1:m
+        % Set settings
+        blur = blur_vec(h);
+        cont = continuity_vec(h);
+
+        % Save settings
+        imaging(i).data(h).blur = blur;
+        imaging(i).data(h).continuity = cont;
+
+        fprintf("blur: %s - continuity: %s | [", string(blur), string(cont))
+        % Preallocate
+        u_est = zeros(length(imgFiles),1);
+        v_est = zeros(length(imgFiles),1);
+        time = 0;
+
+        for k = 1:length(imgFiles)
+            % Find images
+            img_name = imgFiles(k).name;
+            img_path = fullfile(imgFiles(k).folder,img_name);
+            image = imread(img_path);
+
+            % Cropping rectangle
+            r = centerCropWindow2d(size(image),resolution(i,:));
+
+            if blur
+                blur_len = sqrt(u^2+v^2)*exposure_time/dt;
+                blur_angle = rad2deg(atan(v/u));
+                H = fspecial("motion",blur_len,blur_angle);
+                image = imfilter(image,H,"replicate");
+            end
+
+            if cont
+                [original_img, shifted_img] = img_shift(image,u,v);
+                original_img = imcrop(original_img,r);
+                shifted_img = imcrop(shifted_img,r);
+            else
+                image = imcrop(image,r);
+                [original_img, shifted_img] = img_shift(image,u,v);
+            end
+            tic
+            [u_est(k),v_est(k)] = OF(original_img,shifted_img,10);
+            t = toc;
+            time = time + t;
+            fprintf('.');
+        end
+
+        fprintf("]\n")
+        [s_u, m_u] = std(u_est);
+        [s_v, m_v] = std(v_est);
+        merror_u = abs(m_u-u);
+        merror_v = abs(m_v-v);
+        fps = time/length(imgFiles);
+
+        % Save data
+        imaging(i).data(h).u_est = u_est;
+        imaging(i).data(h).v_est = v_est;
+        imaging(i).data(h).mae_u = merror_u;
+        imaging(i).data(h).mae_v = merror_v;
+        imaging(i).data(h).std_u = s_u;
+        imaging(i).data(h).std_v = s_v;
+        imaging(i).data(h).time = time;
+        imaging(i).data(h).fps = fps;
+    end
+end
+
+% Export results as .mat and .json
+save(fullfile(fileparts(mfilename("fullpath")),"OF_reuslts"),"imaging")
+jsonStr = jsonencode(imaging, 'PrettyPrint', true);
+fid = fopen('data.json', 'w');
+if fid == -1, error('Cannot create JSON file.'); end
+fwrite(fid, jsonStr, 'char');
+fclose(fid);
