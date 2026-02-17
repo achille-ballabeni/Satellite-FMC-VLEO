cc9cc8d1ef5d71de5de231f1c0365bc9f7aad572
achille-ballabeni <114033785+achille-ballabeni@users.noreply.github.com>
2026-02-17 16:13:16 +0100

    Update folder name.

--- src/simulations/main.m
+++ src/simulations/main.m
@@ -3,28 +3,32 @@ clear, clc
 %% Orbital parameters and inital conditions
 a = 6378e3 + 250e3; % [m]
 e = 0;
-i = 0;                      % [deg]
+i = 96.4977;                % [deg]
 raan = 237.3113;            % [deg]
 aop = 0;                    % [deg]
 ta = 0;                     % [deg]
 op = [a;e;i;raan;aop;ta];   % orbital parameters
-w0 = [0;0;0];               % initial angular velocity
 startTime = datetime(2025,1,1,12,0,0);
+
+% Initial angular velocity: follow nadir
+mi = 398600.418e9;
+T = period(a,mi);
+w0 = deg2rad([0;360/T;0]);
 
 % Initial nadir-poining attitude: x-axis opposet to velocity and z-axis
 % nadir
 % R3(raan)->R1(i)->R3(aop+ta)
-eci2orb = angle2quat(deg2rad(op(4)),deg2rad(op(3)),deg2rad(op(6)+op(5)),"ZXZ");
+eci2orb = angle2quat(deg2rad(op(4)),deg2rad(op(3)),deg2rad(op(6)+op(5)),"ZXZ"); %TODO: this is wrong if the orbit is constrained to SSO
 % R3(270)->R2(0)->R1(90): Yaw->Pitch->Roll
 orb2body = angle2quat(deg2rad(270),0,deg2rad(90),"ZYX");
 initial_attitude = quatmultiply(eci2orb,orb2body);
 
 %% Run simulation
-timestep = 0.1;
-duration = 10;
+timestep = 0.015;
+duration = 0.15;
 cubesat = satellite_simulation();
 cubesat.set_model_parameters(op, ...
-    true, ...
+    false, ...
     initial_attitude, ...
     w0, ...
     startTime, ...
